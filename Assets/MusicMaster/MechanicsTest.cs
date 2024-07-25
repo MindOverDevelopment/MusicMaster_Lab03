dad6b9b6b777fcc2f467e7e11b470c3a88303c8e
Nibiru Imagineering - Lanny "gilbot" Gilbertson <lannygilbertson@protonmail.com>
2024-07-24 19:59:19 -0700

    Test Branch Init

--- Assets/MusicMaster/MechanicsTest.cs
+++ Assets/MusicMaster/MechanicsTest.cs
@@ -2,12 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LannyTest : MonoBehaviour
+public class MechanicsTest : MonoBehaviour
 {
-    /// <summary>
-    /// Lanny Was here
-    /// </summary>
-
     // Start is called before the first frame update
     void Start()
     {
